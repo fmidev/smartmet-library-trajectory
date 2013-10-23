92b01579838fa0bc975ce52f785e3b748032997a
Mika Heiskanen <mika.heiskanen@fmi.fi>
2013-10-23 09:36:38 +0300

    [qdtrajectory] Added --format option

--- qdtrajectory.cpp
+++ qdtrajectory.cpp
@@ -1,8 +1,23 @@
+#include <boost/algorithm/string.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/program_options.hpp>
 
 #include <stdexcept>
 #include <string>
+
+// ----------------------------------------------------------------------
+/*!
+ * \brief Supported output formats
+ */
+// ----------------------------------------------------------------------
+
+enum OutputFormat
+  {
+	KML,
+	KMZ,
+	ASCII,
+	CSV
+  };
 
 // ----------------------------------------------------------------------
 /*!
@@ -15,6 +30,7 @@ struct Options
   Options();
 
   std::string queryfile;
+  OutputFormat format;
 };
 
 Options options;
@@ -27,8 +43,32 @@ Options options;
 
 Options::Options()
   : queryfile("-")
+  , format(KML)
 {
 }
+
+// ----------------------------------------------------------------------
+/*!
+ * \brief Parse an output format description
+ */
+// ----------------------------------------------------------------------
+
+OutputFormat parse_format(const std::string & desc)
+{
+  std::string name = boost::algorithm::to_lower_copy(desc);
+
+  if(name == "kml")
+	return KML;
+  else if(name == "kmz")
+	return KMZ;
+  else if(name == "ascii")
+	return ASCII;
+  else if(name == "csv")
+	return CSV;
+  else 
+	throw std::runtime_error("Unknown output format '"+desc+"'");
+}
+
 
 // ----------------------------------------------------------------------
 /*!
@@ -42,11 +82,14 @@ bool parse_options(int argc, char * argv[])
   namespace po = boost::program_options;
   namespace fs = boost::filesystem;
 
+  std::string opt_format;
+
   po::options_description desc("Available options");
   desc.add_options()
 	("help,h" , "print out usage information")
 	("version,V" , "display version number")
-	("querydata,q", po::value(&options.queryfile), "input querydata")
+	("querydata,q", po::value(&options.queryfile), "input querydata (standard input)")
+	("format,f", po::value(&opt_format), "output format (kml)")
 	;
 
   po::positional_options_description p;
@@ -78,7 +121,14 @@ bool parse_options(int argc, char * argv[])
 				<< "       qdtrajectory [options] < querydata" << std::endl
 				<< "       cat querydata | qdtrajectory [options]" << std::endl
 				<< std::endl
-				<< "Calculate trajectories based on model level querydata" << std::endl
+				<< "Calculate trajectories based on model level querydata." << std::endl
+				<< std::endl
+				<< "Supported output formats:" << std::endl
+				<< std::endl
+				<< "\tkml\tKeyhole markup language" << std::endl
+				<< "\tkmz\tZipped KML" << std::endl
+				<< "\tascii\tTabular coordinate data" << std::endl
+				<< "\tcsv\tComma separated coordinate data" << std::endl
 				<< std::endl
 				<< desc << std::endl;
 	  return false;
@@ -86,6 +136,9 @@ bool parse_options(int argc, char * argv[])
 
   if(options.queryfile != "-" && !fs::exists(options.queryfile))
 	throw std::runtime_error("Input file '"+options.queryfile+"' does not exist");
+
+  if(!opt_format.empty())
+	options.format = parse_format(opt_format);
 
   return true;
 }
